1f1babb73604bd52c10bde45da9cbb7c2734041e
masoombadkur <masoombadkur48@gmail.com>
2018-07-10 12:57:26 +0530

    # WARNING: head commit changed in the meantime
    Project structure

--- ybsp/ybspWeb/src/main/java/com/yash/ybsp/YbspApp.java
+++ ybsp/ybspWeb/src/main/java/com/yash/ybsp/YbspApp.java
@@ -2,7 +2,6 @@ package com.yash.ybsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,11 +9,6 @@ public class YbspApp extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(YbspApp.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(YbspApp.class);
 	}
 
 }
